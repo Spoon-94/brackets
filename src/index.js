ea5c21ef11126bcfe1e1c0c8de9271069ab1044d
Dima Paymushkin <Dimon-94@yandex.ru>
2020-10-16 10:30:33 +0400

    fix: fix bugs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-   const brackets = bracketsConfig.join('');
+   const brackets = bracketsConfig.join('').replace(/\,/g, ``);
    let stack = [];
    for (let char of str) {
       let bracketsIndex = brackets.indexOf(char);
+
+      if (stack.length > 0 && brackets[bracketsIndex] === brackets[stack[stack.length - 1]]) {
+         stack.pop();
+         continue;
+       }
+
       if(bracketsIndex % 2 === 0) {
          stack.push(bracketsIndex + 1);
       } else {
@@ -11,5 +17,5 @@ module.exports = function check(str, bracketsConfig) {
          }
       }
    }
-   return  stack.length === 0;
+   return stack.length === 0;
 }
