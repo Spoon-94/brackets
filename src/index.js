b602e98bc704b8adc2ac026ba999e85ba43554d7
Dima Paymushkin <Dimon-94@yandex.ru>
2020-10-16 09:42:38 +0400

    feat: complete task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   const brackets = bracketsConfig.join('');
+   let stack = [];
+   for (let char of str) {
+      let bracketsIndex = brackets.indexOf(char);
+      if(bracketsIndex % 2 === 0) {
+         stack.push(bracketsIndex + 1);
+      } else {
+         if(stack.pop() !== bracketsIndex) {
+            return false;
+         }
+      }
+   }
+   return  stack.length === 0;
 }
